b846e45c76d68e8fa1a4345009e6901f72e2aada
Ivan Chuchulski <zake6@abv.bg>
2020-05-25 18:04:30 +0300

    add iterations test

---
+++ project/fractal-parallel-tasks/test-iterations-2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function testIterations() {
+    local output="iterations-350-five-runs.txt"
+	local maxThreads=20
+    local iterations=550
+    local imageSize="800x600"
+
+	truncate -s 0 "${output}"
+    
+	date >> "${output}"
+    echo "maxThreads : ${maxThreads}" >> "${output}"
+    echo "iterations : ${iterations}" >> "${output}"
+
+    for (( thread=1; thread<="${maxThreads}"; thread++ ))
+    do
+        echo "thread : ${thread}" >> "${output}"
+
+        for run in $(seq 5)
+        do 
+            local options="-q -t ${thread} -i ${iterations} -s ${imageSize}"
+            bash ./runMe.sh "${options}" >> "${output}"
+            wait $!
+        done
+        echo "" >> "${output}"
+    done
+}
+
+testIterations
