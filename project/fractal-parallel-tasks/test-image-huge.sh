b0760e6c62b4d6f95c162475f829424487cffac3
Ivan Chuchulski <zake6@abv.bg>
2020-05-25 18:09:30 +0300

    add image testing script

---
+++ project/fractal-parallel-tasks/test-image-huge.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function testImageHuge() {
+    local output="output-big-image-five-runs.txt"
+	local maxThreads=20
+    local imageSize="1920x1440"
+	    
+	truncate -s 0 "${output}"
+    
+	date >> "${output}"
+    echo "maxThreads : ${maxThreads}" >> "${output}"
+    echo "imageSize : ${imageSize}" >> "${output}"
+
+    for thread in $(seq "${maxThreads}")
+    do
+        echo "thread : ${thread}" >> "${output}"
+
+        for run in $(seq 5)
+        do 
+            local options="-q -t ${thread} -s ${imageSize}"
+            bash ./runMe.sh "${options}" >> "${output}"
+            wait $!
+        done
+        echo "" >> "${output}"
+    done
+}
+
+testImageHuge
