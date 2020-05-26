9b86d67703099b68beb21d2a99c46a24ff2dd238
Ivan Chuchulski <zake6@abv.bg>
2020-05-26 20:57:40 +0300

    add scripts to fixed and sequential

--- project/fractal-parallel-sequential-lines/test-rectangle-3.sh
+++ project/fractal-parallel-sequential-lines/test-rectangle-3.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-function testRectanle() {
+function testRectangle() {
     local output="rectangle-3-five-runs.txt"
-	local maxThreads=20
+	local maxThreads=24
     local rectangle="-0.75:0.75:0.0:1.25"
+    local imageSize="1280x960"
 	    
 	truncate -s 0 "${output}"
     
@@ -11,13 +12,22 @@ function testRectanle() {
     echo "maxThreads : ${maxThreads}" >> "${output}"
     echo "rectangle 3 : ${rectangle}" >> "${output}"
 
-    for (( thread=1; thread<="${maxThreads}"; thread++ ))
+    echo "thread : 1" >> "${output}"
+    for run in $(seq 5)
+    do 
+        local options="-q -t 1 -r ${rectangle} -s ${imageSize}"
+        bash ./runMe.sh "${options}" >> "${output}"
+        wait $!
+    done
+    echo "" >> "${output}"
+
+    for (( thread=2; thread<="${maxThreads}"; thread+=2 ))
     do
         echo "thread : ${thread}" >> "${output}"
 
         for run in $(seq 5)
         do 
-            local options="-q -t ${thread} -r ${rectangle}"
+            local options="-q -t ${thread} -r ${rectangle} -s ${imageSize}"
             bash ./runMe.sh "${options}" >> "${output}"
             wait $!
         done
@@ -25,4 +35,4 @@ function testRectanle() {
     done
 }
 
-testRectanle
+testRectangle
