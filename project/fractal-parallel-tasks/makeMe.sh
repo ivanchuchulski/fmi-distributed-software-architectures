5761f40c637c2eae3ea28c37bf21d272b7944fb4
ivanchuchulski <zake6@abv.bg>
2020-06-10 20:43:35 +0300

    add image tests to tasks

--- project/fractal-parallel-tasks/makeMe.sh
+++ project/fractal-parallel-tasks/makeMe.sh
@@ -2,4 +2,4 @@
 
 _CMD_JAVAC=`which javac`
 
-$_CMD_JAVAC -classpath "./lib/commons-math3-3.5.jar:./lib/commons-cli-1.4.jar" -d ./bin ./src/Main.java ./src/FractalPartThread.java
+$_CMD_JAVAC -classpath "./lib/commons-math3-3.5.jar:./lib/commons-cli-1.4.jar" -d ./bin ./src/Main.java ./src/FractalPartThread.java ./src/RowTask.java
